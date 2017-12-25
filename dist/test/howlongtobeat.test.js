26ef803d7f52acfcb1284df08a5935c07149eb40
Christian Katzorke <c.katzorke@web.de>
2017-12-25 19:59:19 +0100

    add tests for detail

--- dist/test/howlongtobeat.test.js
+++ dist/test/howlongtobeat.test.js
@@ -16,7 +16,7 @@ describe('Testing HowLongToBeatParser', () => {
             assert.strictEqual(perc, .9);
         });
     });
-    describe('Test for parseSearch, if this succeeds, butlive installment fails, howlongtobeat.com may have changed their html', () => {
+    describe('Test for parseSearch, if this succeeds, but live installment fails, howlongtobeat.com may have changed their html', () => {
         it('should parse the search result (static, from search of Persona 4)', () => {
             let html = fs.readFileSync('src/test/resources/search.html', 'utf-8');
             let results = howlongtobeat_1.HowLongToBeatParser.parseSearch(html, 'Persona 4');
@@ -26,6 +26,17 @@ describe('Testing HowLongToBeatParser', () => {
             //
             assert.strictEqual(results[2].gameplayCompletionist, 18.5);
             assert.strictEqual(results[4].gameplayMain, 10);
+        });
+    });
+    describe('Test for parseDetail, if this succeeds, but live installment fails, howlongtobeat.com may have changed their html', () => {
+        it('should parse the details page  (static, from id=3978 - God of War 3)', () => {
+            let html = fs.readFileSync('src/test/resources/detail.html', 'utf-8');
+            let detail = howlongtobeat_1.HowLongToBeatParser.parseDetails(html, '3978');
+            assert.isDefined(detail);
+            assert.strictEqual(detail.name, 'God of War III');
+            assert.strictEqual(detail.similarity, 1);
+            assert.strictEqual(detail.gameplayCompletionist, 17);
+            assert.strictEqual(detail.gameplayMain, 10);
         });
     });
 });
--- src/test/howlongtobeat.test.ts
+++ src/test/howlongtobeat.test.ts
@@ -23,7 +23,7 @@ describe('Testing HowLongToBeatParser', () => {
 
   });
 
-  describe('Test for parseSearch, if this succeeds, butlive installment fails, howlongtobeat.com may have changed their html', () => {
+  describe('Test for parseSearch, if this succeeds, but live installment fails, howlongtobeat.com may have changed their html', () => {
     it('should parse the search result (static, from search of Persona 4)', () => {
       let html = fs.readFileSync('src/test/resources/search.html', 'utf-8')
       let results = HowLongToBeatParser.parseSearch(html, 'Persona 4');
@@ -33,6 +33,18 @@ describe('Testing HowLongToBeatParser', () => {
       //
       assert.strictEqual(results[2].gameplayCompletionist, 18.5);
       assert.strictEqual(results[4].gameplayMain, 10);
+    });
+  });
+
+  describe('Test for parseDetail, if this succeeds, but live installment fails, howlongtobeat.com may have changed their html', () => {
+    it('should parse the details page  (static, from id=3978 - God of War 3)', () => {
+      let html = fs.readFileSync('src/test/resources/detail.html', 'utf-8')
+      let detail  = HowLongToBeatParser.parseDetails(html, '3978');
+      assert.isDefined(detail);
+      assert.strictEqual(detail.name, 'God of War III');
+      assert.strictEqual(detail.similarity, 1);
+      assert.strictEqual(detail.gameplayCompletionist, 17);
+      assert.strictEqual(detail.gameplayMain, 10);
     });
   });
 

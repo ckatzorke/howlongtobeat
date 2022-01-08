ffea53dd196811bbc31c8532375be657a58c49fa
GamerKingFaiz <gamerkingfaiz@gmail.com>
2022-01-07 19:43:21 -0800

    Fix

--- dist/main/howlongtobeat.js
+++ dist/main/howlongtobeat.js
@@ -137,7 +137,7 @@ class HowLongToBeatParser {
             let liElements = $('li');
             liElements.each(function () {
                 let gameTitleAnchor = $(this).find('a')[0];
-                let gameName = $(this).find('a')[1].text();
+                let gameName = $(this).find('a')[1].text;
                 const gameDescription = '';
                 const platforms = [];
                 let detailId = gameTitleAnchor.attribs.href.substring(gameTitleAnchor.attribs.href.indexOf('?id=') + 4);
--- src/main/howlongtobeat.ts
+++ src/main/howlongtobeat.ts
@@ -161,7 +161,7 @@ export class HowLongToBeatParser {
       let liElements = $('li');
       liElements.each(function() {
         let gameTitleAnchor = $(this).find('a')[0];
-        let gameName: string = $(this).find('a')[1].text();
+        let gameName: string = $(this).find('a')[1].text;
         const gameDescription = '';
         const platforms = [];
         let detailId: string = gameTitleAnchor.attribs.href.substring(
